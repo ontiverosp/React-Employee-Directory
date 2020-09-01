69ed9fc7682b48a71c5e9c1471acfd5d5670d82f
Pablo Ontiveros <pablacas@gmail.com>
2020-09-01 11:53:35 -0500

    filter by first name

--- employee-directory/src/components/Table/index.js
+++ employee-directory/src/components/Table/index.js
@@ -8,39 +8,57 @@ class Table extends React.Component {
 
   componentDidMount() {
     this.setState({ employees: data })
-    console.log(this.state.employees)
   }
 
-  sort(value){
-    // console.log(this.state.employees)
+  sort(value) {
     const tempEmployees = this.state.employees
-    tempEmployees.sort((a,b) => (a[value] > b[value]) ? 1 : ((b[value] > a[value]) ? -1 : 0));
-    this.setState({employees: tempEmployees})
-    // console.log(this.state.employees)
+    tempEmployees.sort((a, b) => (a[value] > b[value]) ? 1 : ((b[value] > a[value]) ? -1 : 0));
+    this.setState({ employees: tempEmployees })
+  }
+
+  filter(value) {
+    let filterTerm = value.target.value;
+    if (filterTerm === ''){
+      this.setState({employees: data});
+      return ;
+    }
+    let i = 0;
+    const allEmployees = data;
+    const tempEmployees = [];
+    for(i=0;i<allEmployees.length;i++){
+      if (allEmployees[i].first.toLowerCase() === filterTerm.toLowerCase()){
+        tempEmployees.push(allEmployees[i])
+      }
+    }
+    console.log(tempEmployees)
+    this.setState({ employees: tempEmployees })
   }
 
   render() {
     const data = this.state.employees;
-    console.log(data)
     return (
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th onClick={()=>this.sort('first')}>First</th>
-              <th onClick={()=>this.sort('last')}>Last</th>
-              <th onClick={()=>this.sort('email')}>Email</th>
-              <th onClick={()=>this.sort('phone')}>Phone</th>
-              <th onClick={()=>this.sort('dep')}>Dep</th>
-              <th onClick={()=>this.sort('position')}>Position</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((employees, index) => (
-              <Row key={index} employee={employees} />
-            ))}
-          </tbody>
-        </table>
+        <form>
+          <label for="search">First name:</label>
+          <input type="text" id="search" onChange={(event) => this.filter(event)}></input>
+        </form>
+          <table>
+            <thead>
+              <tr>
+                <th onClick={() => this.sort('first')}>First</th>
+                <th onClick={() => this.sort('last')}>Last</th>
+                <th onClick={() => this.sort('email')}>Email</th>
+                <th onClick={() => this.sort('phone')}>Phone</th>
+                <th onClick={() => this.sort('dep')}>Dep</th>
+                <th onClick={() => this.sort('position')}>Position</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((employees, index) => (
+                <Row key={index} employee={employees} />
+              ))}
+            </tbody>
+          </table>
       </div>
     )
   };
