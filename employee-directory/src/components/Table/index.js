664577303c03e44612e92a9e151cb31590d50e78
Pablo Ontiveros <pablacas@gmail.com>
2020-09-01 09:12:20 -0500

    set up

---
+++ employee-directory/src/components/Table/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Table extends React.Component {
+  render(){
+    <div>
+      <table>
+  <tr>
+    <th>First</th>
+    <th>Last</th>
+    <th>Email</th>
+    <th>Phone</th>
+    <th>Dep</th>
+    <th>Position</th>
+  </tr>
+  <tr>
+    <td>Bob</td>
+    <td>Lance</td>
+    <td>bob@email.com</td>
+    <td>1234567890</td>
+    <td>Sales</td>
+    <td>Sales Manager</td>
+  </tr>
+</table>
+    </div>
+  }; 
+}
+
+export default Table;
