833b90f84d72075962ad7df8f29864c3daf0f667
Pablo Ontiveros <pablacas@gmail.com>
2020-09-01 12:51:18 -0500

    build app

---
+++ build/precache-manifest.0165193d1a2676141f42962cdbc3cf1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105d7832e031380c86be06cf1a860277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89de6c8bdf629b456c8b",
+    "url": "/static/css/main.be08f376.chunk.css"
+  },
+  {
+    "revision": "a147b6f1d73b7d5c403f",
+    "url": "/static/js/2.99627b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99627b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89de6c8bdf629b456c8b",
+    "url": "/static/js/main.a4a4043b.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
